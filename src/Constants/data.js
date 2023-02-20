99727dd348d2fd3a306c12c55376e24b31de6408
= <=>
2023-02-20 22:07:13 +0500

    added dummy JSON data

---
+++ src/Constants/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        "id": "B08C74KDPJ",
+        "title": "Apple iPhone 11, 64GB, Purple - Fully Unlocked (Renewed)",
+        "image": "https://m.media-amazon.com/images/I/71xn9bCRfhL._AC_UY218_.jpg",
+        "price": 599.99,
+        "rating": 4
+    },
+    {
+        "id": "B08FJ8ZP5N",
+        "title": "Samsung Galaxy S21 5G | Factory Unlocked Android Cell Phone | US Version 5G Smartphone | Pro-Grade Camera, 8K Video, 64MP High Res | 128GB, Phantom Gray (SM-G991UZAAXAA)",
+        "image": "https://m.media-amazon.com/images/I/41puyo+i8bL.jpg",
+        "price": 799.99,
+        "rating": 5
+    },
+    {
+        "id": "B07YV1F6NW",
+        "title": "Amazon Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal",
+        "image": "https://m.media-amazon.com/images/I/61u48FEs0rL._AC_UY218_.jpg",
+        "price": 39.99,
+        "rating": 4
+    },
+    {
+        "id": "B086MK55XK",
+        "title": "Apple iPad Pro (11-inch, Wi-Fi, 128GB) - Space Gray (2nd Generation)",
+        "image": "https://cdn.cimri.io/image/1000x1000/appleipadpromxdetuagbwifiinuzaygrisi_292849664.jpg",
+        "price": 749.00,
+        "rating": 5
+    },
+    {
+        "id": "B08KGZ79N3",
+        "title": "Sony Playstation 5 Console Bundle with Marvel's Spider-Man: Miles Morales Ultimate Edition",
+        "image": "https://i5.walmartimages.com/asr/0895e590-fd32-4ca2-a0e9-020824b6b1a5.8362a04dd6c966b4781c7830a162cb72.jpeg",
+        "price": 799.99,
+        "rating": 5
+    }
+]
